24c1f6f74e8b08832121a7d8028ed45ac26613c7
OyShan1 <ilhamdzaky2007@gmail.com>
2024-11-07 15:16:51 +0700

    newest

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      width: {
+        150: "150px",
+      },
+      backgroundColor: {
+        default: "#D9D9D9",
+        navColor: "#2A2828",
+      },
       backgroundImage: {
         "login-pg": "url('/dist/img/login-bg.jpeg')",
       },
